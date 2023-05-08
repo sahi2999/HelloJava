28d84eb2ef98dac5a16e8f72b41a13172a2aa03f
pesahity <pesahity@inlen8520088251.corp.capgemini.com>
2023-05-08 12:44:52 +0530

    Sample commit

---
+++ src/Hello/HelloJava.java
@@ -0,0 +1,9 @@
+package Hello;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+      System.out.println("Hello world");
+	}
+
+}
---
+++ src/HiiJava.java
@@ -0,0 +1,8 @@
+
+public class HiiJava {
+
+	public static void main(String[] args) {
+    System.out.println("Hii Java");
+	}
+
+}
